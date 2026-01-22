51b583ade2d8e85685518d290dd6f170997b9b0a
Мурад Гусейнли <muradhuseynli29@gmail.com>
2026-01-23 02:00:27 +0400

    Fix typescript config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,21 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+import { resolve, dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default defineConfig({
   plugins: [tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        shop: resolve(__dirname, "shop.html"),
+        about: resolve(__dirname, "about.html"),
+        contact: resolve(__dirname, "contact.html"),
+      },
+    },
+  },
 });
